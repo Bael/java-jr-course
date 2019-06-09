2bdd21c85e579a0878fd91c546095c60c28f294d
Denis <denkern@gmail.com>
2019-06-09 11:57:27 +0700

    unit-9 xml example with two providers

--- unit-9/xml/src/main/java/io/github/bael/xml/DogShop.java
+++ unit-9/xml/src/main/java/io/github/bael/xml/DogShop.java
@@ -1,5 +1,8 @@
 package io.github.bael.xml;
 
+import org.springframework.context.annotation.Primary;
+
+@Primary
 public class DogShop implements PetService {
     @Override
     public Pet adorablePet() {
--- unit-9/xml/src/main/java/io/github/bael/xml/XMLApplication.java
+++ unit-9/xml/src/main/java/io/github/bael/xml/XMLApplication.java
@@ -7,10 +7,12 @@ public class XMLApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ApplicationXMLConfig.xml");
         PetService service = applicationContext.getBean(PetService.class);
-        PetService serviceByName = (PetService) applicationContext.getBean("PetService");
+        PetService serviceCat = (PetService) applicationContext.getBean("PetServiceCat");
+        PetService serviceDog = (PetService) applicationContext.getBean("PetServiceDog");
 
 
-        System.out.println(service.adorablePet().speak());
-        System.out.println(serviceByName.adorablePet().speak());
+        System.out.println(serviceDog.adorablePet().speak());
+        System.out.println(serviceCat.adorablePet().speak());
+        System.out.println("default: " + service.adorablePet().speak());
     }
 }
